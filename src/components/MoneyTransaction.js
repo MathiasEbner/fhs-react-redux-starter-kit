73959f933d3a79d3ab5748ef044073e208a6d536
Mathias Ebner <mebner.mmt-b2021@fh-salzburg.ac.at>
2023-03-22 01:15:24 +0100

    add fetch with hooks

--- src/components/MoneyTransaction.js
+++ src/components/MoneyTransaction.js
@@ -1,20 +1,32 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import styles from './MoneyTransaction.module.css'
 import { MoneyTransactionCreate } from './MoneyTransactionCreate'
 import { MoneyTransactionList } from './MoneyTransactionList'
 
-export const MoneyTransaction = () => {
-  const users = [
-    { name: 'Mathias', id: 1 },
-    { name: 'Lukas', id: 2 },
-    { name: 'Simon', id: 3 }
-  ]
+const useHttpRequest = (endpoint) => {
+  const [response, setResponse] = useState([])
+  const [loading, setLoading] = useState(true)
 
-  const transactions = [
-    { id: 1, creditorId: 1, debitorId: 2, amount: 10.00, paidAt: null },
-    { id: 2, creditorId: 3, debitorId: 1, amount: 11.20, paidAt: '2000-01-01T00:00:00+01+00' },
-    { id: 3, creditorId: 2, debitorId: 3, amount: 8.40, paidAt: null }
-  ]
+  useEffect(() => {
+    setLoading(true)
+    fetch(endpoint)
+      .then((response) => response.json())
+      .then((json) => {
+        setResponse(json)
+        setLoading(false)
+      })
+  }, [])
+  return { response, loading }
+}
+
+export const MoneyTransaction = () => {
+  const { response: transactions, loading: transactionsLoading } = useHttpRequest('http://localhost:3001/money-transaction')
+  const { response: users, loading: usersLoading } = useHttpRequest('http://localhost:3001/user')
+
+  if (transactionsLoading || usersLoading) {
+    // Render a loading indicator while data is being fetched
+    return <div>Loading...</div>
+  }
 
   return (
     <main className={styles.container}>
